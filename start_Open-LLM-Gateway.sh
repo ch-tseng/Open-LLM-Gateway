79ecb46a0a135b100a24fdb0d8758a6757e051ba
Tseng Cheng Hsun <ch.tseng@sunplusit.com>
2025-05-20 17:51:42 +0800

    Update start_Open-LLM-Gateway.sh

--- start_Open-LLM-Gateway.sh
+++ start_Open-LLM-Gateway.sh
@@ -1,3 +1,2 @@
-source ~/envs/openLLM/bin/activate
-cd /GPUData/projs/Open-LLM-Gateway
+echo "伺服器正在啟動，第一次執行或依賴較多時可能需要較長時間，請耐心等待直到看見 'Application startup complete.' ..."
 uvicorn main:app --reload --host 0.0.0.0 --port 8000
